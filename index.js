bfc4fff6b91da3bd7078467ab86e8ad6a29dfe52
Aaqib2121 <aaqibnakwa4913@gmail.com>
2021-10-26 19:13:50 +0530

    DOJO-2

---
+++ index.js
@@ -0,0 +1,16 @@
+// $(document).ready(function(){
+//     $('#frmSend').on('send', function (e) {
+//         e.preventDefault();
+//         $.ajax ({
+//             url : 'https://script.google.com/macros/s/AKfycby-QrRJoc2bHi4OVb-n3VYFbU4_7jAIL4BdjiMW9S4XNxjvvdN-A9mvmbIvavFw4IK3bQ/exec',
+//             type: 'post',
+//             data : $('#frmSend').serialize(),
+//             success : function (result) {
+//                 console.log(result);
+//             }
+//         });
+//     });
+// });
+
+
+
