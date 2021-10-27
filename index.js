c5897aa0e7e7ba206e616589028990ba12248b08
Aaqib2121 <aaqibnakwa4913@gmail.com>
2021-10-27 15:18:50 +0530

    DOJO-2

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-// $(document).ready(function(){
-//     $('#frmSend').on('send', function (e) {
-//         e.preventDefault();
-//         $.ajax ({
-//             url : 'https://script.google.com/macros/s/AKfycby-QrRJoc2bHi4OVb-n3VYFbU4_7jAIL4BdjiMW9S4XNxjvvdN-A9mvmbIvavFw4IK3bQ/exec',
-//             type: 'post',
-//             data : $('#frmSend').serialize(),
-//             success : function (result) {
-//                 console.log(result);
-//             }
-//         });
-//     });
-// });
 
 
 
+$('#frmSend').on('submit',function (e) {
+    e.preventDefault();
+    alert('successfull');
+    $.ajax({
+        url : 'https://script.google.com/macros/s/AKfycbziGvwFfpMbpTbMrdtF0gIbtdgQKx_Rjit5f6XwkPriy0WR9vZdYfy8p5ABjtRCf5Ly/exec',
+        type : 'post',
+        data : $('#frmSend').serialize(),
+        success : function(result){
+            console.log(result);
+        }
+    });  
+});
+
+
+const counters = document.querySelectorAll('.counter');
+const speed = 200;
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target  = +counter.getAttribute('data-target');
+
+        const count = +counter.innerText
+
+        const inc = target/speed;
+
+        if (count < target) {
+            counter.innerText = count + inc;
+            setTimeout(updateCount, 1);
+        }else{
+            count.innerText =target;
+        }
+    }
+
+    updateCount();
+});
