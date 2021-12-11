252ae921b80d6d200b3dc8ac4cc9f6ae2f7dfdb1
Aaqib2121 <aaqibnakwa4913@gmail.com>
2021-12-11 17:54:22 +0530

    DOJO-2

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 
 
 
-$('#frmSend').on('submit',function (e) {
-    e.preventDefault();
-    alert('successfull');
-    $.ajax({
-        url : 'https://script.google.com/macros/s/AKfycbziGvwFfpMbpTbMrdtF0gIbtdgQKx_Rjit5f6XwkPriy0WR9vZdYfy8p5ABjtRCf5Ly/exec',
-        type : 'post',
-        data : $('#frmSend').serialize(),
-        success : function(result){
-            console.log(result);
-        }
-    });  
-});
+
 
 
 const counters = document.querySelectorAll('.counter');
